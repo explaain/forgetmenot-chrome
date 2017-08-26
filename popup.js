c146aed9fc40a0795fdbc1710c4c8dd3a068b75e
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-08-26 20:49:38 +0100

    v3

--- popup.js
+++ popup.js
@@ -1,36 +1,104 @@
-const client = algoliasearch('I2VKMNNAXI', '2b8406f84cd4cc507da173032c46ee7b');
-const index = client.initIndex('ForgetMeNot_Context');
+const AlgoliaClient = algoliasearch('I2VKMNNAXI', '2b8406f84cd4cc507da173032c46ee7b');
+const AlgoliaIndex = AlgoliaClient.initIndex('ForgetMeNot_Context');
 
-// const searchAndDisplay = function(searchText, hitsPerPage) {
-//   console.log(searchText);
-//
-//   index.search(searchText.substring(400), {
-//     hitsPerPage: hitsPerPage
-//   }, function(err, content) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       // $('.abc').html(searchText)
-//       // $('.abc').html(JSON.stringify(content.hits))
-//       console.log(content.hits);
-//       $('.abc').html($('.abc').html() + content.hits.map(function(hit) {
-//         return '<div class="card">'
-//         +     hit.sentence
-//         +   '</div>'
-//       }).join(''));
-//     }
-//   });
-// }
+var app = new Vue({
+  el: '#app',
+  data: {
+    plugin: false,
+    pageCards: [],
+    cards: [],
+    query: '',
+  },
+  methods: {
+    search: function() {
+      const self = this;
+      searchCards(app.query, 12)
+      .then(function(hits) {
+        console.log('hits');
+        self.cards = hits
+      }).catch(function(err) {
+        console.log(err);
+      })
+    },
+    fromPage: function() {
+      if (this.pageCards.length) {
+        this.cards = this.pageCards;
+      } else {
+        getPageText()
+        .then(function(text) {
+          searchAll(text)
+        })
+      }
+    },
+    searchRecent: function() {
+      const self = this;
+      searchCards("", 24)
+      .then(function(hits) {
+        console.log('hits');
+        self.cards = hits
+      }).catch(function(err) {
+        console.log(err);
+      })
+    },
+    create: function() {
+
+    },
+    copytoClipboard: function(element) {
+      console.log(element);
+      console.log(element.target);
+      console.log(this.$el);
+      console.log($(this));
+      element.target.select();
+      document.execCommand('copy');
+    }
+  }
+})
+
+Vue.component('card', {
+  props: ['card'],
+  template: '<div class="card"><p>{{card.sentence}}</p><img v-if="card.attachments && card.attachments[0]" v-bind:src="card.attachments[0].url"></div>'
+})
+
+
+
+
+
+
+
+
+
+
+const createCard = function(text) {
+  // const d = Q.defer()
+  // console.log(searchText);
+  // AlgoliaIndex.addObject({
+  //   firstname: 'Jimmie',
+  //   lastname: 'Barninger'
+  // }, 'myID', function(err, content) {
+  //   console.log('objectID=' + content.objectID);
+  // });
+  // AlgoliaIndex.search(searchText, {
+  //   hitsPerPage: hitsPerPage
+  // }, function(err, content) {
+  //   if (err) {
+  //     console.log(err);
+  //     d.reject(err)
+  //   } else {
+  //     d.resolve(content.hits)
+  //   }
+  // });
+  // return d.promise
+}
 
 const searchCards = function(searchText, hitsPerPage) {
   const d = Q.defer()
   console.log(searchText);
-  index.search(searchText.substring(400), {
+  AlgoliaIndex.search(searchText, {
     hitsPerPage: hitsPerPage
   }, function(err, content) {
     if (err) {
       console.log(err);
-      d.reject()
+      d.reject(err)
     } else {
       d.resolve(content.hits)
     }
@@ -38,13 +106,20 @@ const searchCards = function(searchText, hitsPerPage) {
   return d.promise
 }
 
+/* For Browser Plugin Only */
+const getPageText = function() {
+  const d = Q.defer()
+  d.resolve("My favourite book")
+  // chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
+  //   chrome.tabs.sendMessage(tabs[0].id, {action: "readDom"}, searchAll);
+  // });
+  return d.promise
+}
 const searchAll = function(searchText) {
   console.log(searchText);
   const maxLength = 400;
   const searchTextArray = [];
-  const hitsPerPage = Math.max(Math.ceil(10 / (searchText.length / maxLength)), 3)
-  console.log('hitsPerPage');
-  console.log(hitsPerPage);
+  const hitsPerPage = Math.min(Math.max(Math.ceil(10 / (searchText.length / maxLength)), 3), 12);
   for (var i = 0; i < searchText.length; i += maxLength) {
     searchTextArray.push(searchText.substring(i, i+maxLength))
   }
@@ -55,52 +130,28 @@ const searchAll = function(searchText) {
   });
   Q.allSettled(promises)
   .then(function(results) {
-    console.log('results');
-    console.log(results);
-    var results2 = [].concat.apply([], results.map(function(r) {return r.value}));
-    console.log('results2');
-    console.log(results2);
-    var results3 = removeDuplicates(results2, 'objectID')
-    console.log('results3');
-    console.log(results3);
-    $('.abc').html($('.abc').html() + results3.map(function(hit) {
-      return '<div class="card">'
-      +     hit.sentence
-      +   '</div>'
-    }).join(''));
+    var results = [].concat.apply([], results.map(function(r) {return r.value}));
+    var results = removeDuplicates(results, 'objectID')
+    app.pageCards = results;
+    app.cards = results;
 		d.resolve(results)
 	});
 }
-
-function removeDuplicates(originalArray, objKey) {
-  var trimmedArray = [];
-  var values = [];
-  var value;
-
+const removeDuplicates = function(originalArray, objKey) {
+  var trimmedArray = [], values = [], value;
   for(var i = 0; i < originalArray.length; i++) {
     value = originalArray[i][objKey];
-
     if(values.indexOf(value) === -1) {
       trimmedArray.push(originalArray[i]);
       values.push(value);
     }
   }
-
   return trimmedArray;
 }
-
-
-$(function (){
-  $('#readDom').click(function(){
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
-      chrome.tabs.sendMessage(tabs[0].id, {action: "readDom"}, searchAll);
-    });
-  });
-});
+/* End Browser Plugin */
 
 window.onload = function() {
-  console.log("onload" + Date())
-  chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
-    chrome.tabs.sendMessage(tabs[0].id, {action: "readDom"}, searchAll);
-  });
+  if (app.plugin) {
+    app.fromPage();
+  }
 };
