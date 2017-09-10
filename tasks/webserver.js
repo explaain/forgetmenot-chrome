bf994a626bdadfcf9d7360b1f55980ca0f611c92
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-09-10 17:53:17 +0100

    test5

--- tasks/webserver.js
+++ tasks/webserver.js
@@ -2,11 +2,11 @@ var gulp = require('gulp'),
   connect = require('gulp-connect');
 
 gulp.task('connect', function() {
-  // connect.server({
-  //   root: 'dist/chrome',
-  //   port: process.env.PORT || 8000,
-  //   livereload: false
-  // });
+  connect.server({
+    root: 'dist/chrome',
+    port: process.env.PORT || 8000,
+    livereload: false
+  });
 });
 
-gulp.task('default', ['connect']);
+// gulp.task('default', ['connect']);
