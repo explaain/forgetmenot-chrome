456dc877f42f7859777584c474297f1cf6bd0d5e
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-10-04 00:05:26 +0100

    working?

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!!')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World!!')
+// })
+
+app.use(express.static('dist/chrome'))
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
